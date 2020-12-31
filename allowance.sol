97b761731f74b0234a5967ed115cd45a04dcaa94
SkynetDev <andrejohnson0017@gmail.com>
2020-12-30 23:49:28 -0500

    Added authentication and validation of user inputs

--- allowance.sol
+++ allowance.sol
@@ -1,15 +1,33 @@
 pragma solidity ^0.7.4;
 
-contract allowance {
+contract AllowaDraw {
     
- address owner;
-
-constructor() {
-    owner = msg.sender;
-}
-
-function getOwner() public view returns(address) {
-    return owner;
-}
-
+    address payable owner;
+    
+    mapping(address => uint) public Allowance;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    function getBalance() public view returns(uint) {
+      return address(this).balance;
+    }
+    
+    function depositMoney() public payable {
+        
+    }
+    
+    function giveAllowance(address payable _to, uint _amount) public {
+        require(msg.sender == owner, "You are not the owner");
+        assert((address(this).balance - _amount) <= address(this).balance);
+        assert((Allowance[_to] + _amount) >= Allowance[_to]);
+        address(this).balance - _amount;
+        Allowance[_to] + _amount;
+    }
+    
+    function witdrawMoney(address payable _to, uint _amount) public {
+        
+    }
+    
 }
