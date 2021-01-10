1a514f5464b18b35e4c947931f9dcec46b03a839
SkynetDev <andrejohnson0017@gmail.com>
2021-01-09 23:52:05 -0500

    Modularized the smart contracts to improve auditability

--- allowance.sol
+++ allowance.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.7.4;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract AllowaDraw is Ownable {
+contract AllowanceContract is Ownable {
     
     mapping(address => uint) public allowance;
     
@@ -21,23 +21,24 @@ contract AllowaDraw is Ownable {
       return address(this).balance;
     }
     
-    function depositMoney() public payable {
-        allowance[ownerallow] += msg.value;
-    }
-    
-    function giveAllowance(address _who, uint _amount) public onlyOwner {
+     function giveAllowance(address _who, uint _amount) public onlyOwner {
         allowance[_who] = _amount;
-        
-        // assert((Allowance[msg.sender] - _amount) <= Allowance[msg.sender]);
-        // assert((Allowance[_to] + _amount) >= Allowance[_to]);
-        // address(this).getBalance() -= _amount;
-        // Allowance[_to] += _amount;
         
     }
     
    function reduceAllowance(address _who, uint _amount) internal {
        allowance[_who] -= _amount;
    }
+}
+
+contract AllowaDraw is AllowanceContract {
+    
+    
+    function depositMoney() public payable {
+        allowance[ownerallow] += msg.value;
+    }
+    
+   
     
     function witdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount){
         require(_amount <= address(this).balance , "There are not enough funds on the smart contract");
